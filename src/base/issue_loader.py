d864a9a199df3b8c40f22d7c33696c748b16ac2e
JeongSH1 <jyt001532@gmail.com>
2024-07-11 14:28:18 +0900

    sub: IssueLoader의 Crawler 상속 및 초기화 클래스 작성

---
+++ src/base/issue_loader.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+from src.base import Crawler
+
+
+class IssueLoader(Crawler, ABC):
+
+    def __init__(self):
+        super().__init__()
+        self.target_platforms = {}
+        self.main_platform = 'unknown'
