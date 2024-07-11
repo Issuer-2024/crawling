16f8cfb9323802a5f0e1b3865db6cea32a13750b
JeongSH1 <jyt001532@gmail.com>
2024-07-11 14:31:12 +0900

    feat: issue들을 crawling하는 메서드 작성, 가져온 이슈에 대한 메타 데이터를 추가

--- src/base/issue_loader.py
+++ src/base/issue_loader.py
@@ -12,3 +12,16 @@ class IssueLoader(Crawler, ABC):
     @abstractmethod
     def _parse(self, html):
         pass
+
+    def crawl_issues(self):
+        all_issues = []
+        for platform, url in self.target_platforms.items():
+            self._fetch_page(url)
+            issue_items = self._parse(self.driver.page_source)
+            for i in range(len(issue_items)):
+                issue_items[i]['플랫폼'] = platform
+                issue_items[i]['상위 플랫폼'] = self.main_platform
+                issue_items[i]['하위 플랫폼'] = platform
+            all_issues += issue_items
+        self._close()
+        return all_issues
