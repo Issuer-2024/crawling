76bf311217a7686f2c7debe9e04f6606b21328e5
JeongSH1 <jyt001532@gmail.com>
2024-07-11 14:37:26 +0900

    feat: 본문을 크롤링하는 메서드 구현 (페이지 접속 후 크롤링, 드라이버 종료)

--- src/base/content_crawler.py
+++ src/base/content_crawler.py
@@ -8,5 +8,9 @@ class ContentCrawler(Crawler, ABC):
     def _parse(self, html):
         pass
 
-
+    def crawl_content(self, url):
+        self._fetch_page(url)
+        content_data = self._parse(self.driver.page_source)
+        self._close()
+        return content_data
 
