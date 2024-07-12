4d73f5e9a23a3a2ed62535fd139a8114a18cbeb1
JeongSH1 <jyt001532@gmail.com>
2024-07-12 15:16:17 +0900

    project: docker-compose 수정

--- src/file_manager/file_manager.py
+++ src/file_manager/file_manager.py
@@ -17,8 +17,8 @@ class FileManager:
             'ISSUE_COMMENTS': ['문서 번호', '작성자', '내용', '추천 수', '비추천 수', '대댓글 수', '작성 시간']}
 
     def save(self, data, main_type, sub_type):
-        (logger.info
-         (f"[COLLECTED DATA] {data}"))
+        if not data:
+            return
 
         try:
             current_datetime = datetime.now().strftime("%Y-%m-%d_%H-00-00")
